1f1a8111f75b7309d2dda05251393cfc6735682e
AgnGra <116488439+AgnGra@users.noreply.github.com>
2022-10-30 15:18:22 +0100

    z JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const cardTitle=document.querySelector("h1");
+const cardSubtitle=document.querySelector("h3")
+cardTitle.textContent=("Dzień dobry");
+cardSubtitle.textContent=("Cześć Janek");
+
+
+
+
+
+
+// const cardTitle = document.createElement("h2");
+// cardTitle.classList.add("klasa1");
+// cardTitle.textContent="Buty zimowe";
